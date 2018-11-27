66c7449a1249f8ba340c902cf3becc9ec5115608
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2018-11-27 13:45:27 +0100

    Activity Principal

--- app/src/main/java/com/alvar/practica4pmdm/MainActivity.java
+++ app/src/main/java/com/alvar/practica4pmdm/MainActivity.java
@@ -1,7 +1,10 @@
 package com.alvar.practica4pmdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 0;
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), NumerosPrimos.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
